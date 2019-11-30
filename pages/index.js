d3ea61c7e4dca3a693168f199cece015efe399ce
abenedi4 <abenedi4@uncc.edu>
2019-11-29 23:19:34 -0500

    test Links with title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Index = () => (
     <div>
-        <Link href="/about">
+        <Link href="/about" title="About Page">
             <a>About Page</a>
         </Link>
         <p>Hello Next.js</p>
