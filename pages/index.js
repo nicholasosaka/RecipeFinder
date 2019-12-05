f54dd498878e37902798efcfdde2cb58523f96dd
Nikko Osaka <nikko.osaka99@gmail.com>
2019-12-05 15:35:08 -0500

    some more changes

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,20 @@ import React from 'react';
 
 const PostLink = props => (
     <li>
-        <Link href={`/recipe?title=${props.title}`}>
-            <a>{props.title}</a>
+        <Link href="/recipe/[id]" as={`/recipe/${props.id}`}>
+            <a>{props.id}</a>
         </Link>
     </li>
 );
+
 export default function Blog() {
     return (
         <Layout>
             <h1>My Blog</h1>
             <ul>
-                <PostLink title="Hello Next.js" />
-                <PostLink title="Learn Next.js is awesome" />
-                <PostLink title="Deploy apps with Zeit" />
+                <PostLink id="hello-nextjs" />
+                <PostLink id="learn-nextjs" />
+                <PostLink id="deploy-nextjs" />
             </ul>
         </Layout>
     );
