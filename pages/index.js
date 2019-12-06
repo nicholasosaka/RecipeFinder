64f1d6a50447d83383f58ad5b5e59730809ce818
Nikko Osaka <nikko.osaka99@gmail.com>
2019-12-06 10:26:17 -0500

    new stuff

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '../components/MyLayout';
-import data from '../datasets/epirecipes/full_format_recipes';
 import fetch from 'isomorphic-unfetch';
+import Link from 'next/Link'
+import Form from '../components/Form';
 
-const Index = () => (
+const Index = (props) => (
     <Layout>
         <h1>Recipe Finder</h1>
-        <p>Enter the ingredients that you want to use (comma seperated):</p>
-        <form>
-            <input type="text" name="Ingredients"/>
-            <input type="submit" value="Search"/>
-        </form>
+        <p>Enter the ingredients that you want to use (comma separated):</p>
+        <Form />
+
+        <p>FOR DEBUG ONLY</p>
+        <ul>
+            {props.recipes.map(recipe =>(
+                <li key={recipe.title}>
+                    <Link href="/recipe/[id]" as={`/recipe/${recipe.title}`}>
+                        <a>{recipe.title}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
     </Layout>
 );
 
-Index.getInitialProps = async function(){
+Index.getInitialProps = async function() {
     const res = await fetch('https://raw.githubusercontent.com/ProjectRecipe/RecipeFinder/master/datasets/clean_recipes.json');
     const data = await res.json();
 
-    console.log('found data: ' + data.length)
+    console.log(res);
+    console.log(`Recipe data fetched. Count: ${data.length}`);
+
     return {
         recipes: data
-    }
+    };
 };
+
 
 export default Index;
