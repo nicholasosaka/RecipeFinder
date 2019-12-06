d12510002c302100a78ad1745c8e2633836e5c82
Nikko Osaka <nikko.osaka99@gmail.com>
2019-12-06 12:53:57 -0500

    dynamic page generation

--- pages/recipe/[id].js
+++ pages/recipe/[id].js
@@ -5,9 +5,37 @@ import fetch from 'isomorphic-unfetch';
 const Post = props => (
     <Layout>
         <h1>{props.recipe.title}</h1>
+        <p>Rating: {props.recipe.rating}</p>
         <p>{props.recipe.desc}</p>
         <br/>
-        <p>Directions: {props.recipe.title}</p>
+        <h3>Directions:</h3>
+        <ul>
+            {props.recipe.directions.map(item =>
+            <li>
+                {item}
+            </li>)}
+        </ul>
+        <br/>
+        <div id="nutitional">
+            <h3>Nutritional Information</h3>
+            <p>{props.recipe.calories} Calories</p>
+            <p>{props.recipe.protein}g Protein</p>
+            <p>{props.recipe.fat}g Fat</p>
+            <p>{props.recipe.sodium}g Sodium</p>
+        </div>
+        <div id="ingredients">
+            <h3>Ingredients</h3>
+            <ul>
+                {props.recipe.ingredients.map(item =>
+                <li>
+                    {item}
+                </li>)}
+            </ul>
+        </div>
+        <br/><br/><br/><br/>
+        <div id="categories">
+            {props.recipe.categories.map(item => item + ", ")}
+        </div>
     </Layout>
 );
 
