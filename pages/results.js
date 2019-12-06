1e2c17e61629000fb15ddc4c789007a3c0067971
Nikko Osaka <nikko.osaka99@gmail.com>
2019-12-06 13:07:01 -0500

    remove broken code for now

--- pages/results.js
+++ pages/results.js
@@ -4,15 +4,6 @@ import Link from 'next/link';
 const Results = props => (
     <Layout>
         <h1>Recipes</h1>
-        <ul>
-            {props.recipes.map(recipes => (
-                <li key={recipes.id}>
-                    <Link href="/recipe/[id]" as={`/recipe/${recipes.id}`}>
-                        <a>{recipes.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
     </Layout>
 );
 
